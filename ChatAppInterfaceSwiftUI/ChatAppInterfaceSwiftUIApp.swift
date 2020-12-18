0e4809132f3f60f3bddbb2e5f6336371140cf77e
Nguyen Trong Phat <phatnt@rakumo.vn>
2020-12-18 14:05:07 +0700

    Initial Commit

---
+++ ChatAppInterfaceSwiftUI/ChatAppInterfaceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppInterfaceSwiftUIApp.swift
+//  ChatAppInterfaceSwiftUI
+//
+//  Created by phatnt on 12/18/20.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppInterfaceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
